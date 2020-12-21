4dc7246180120b912b3c29a19327b2036cc61fa6
= <=>
2020-12-21 23:39:23 +0600

    testing coleb

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        // Category::truncate();
+        Product::truncate();
     }
 }
